ee4a134417dd09d5b52f47fd04699bdf10099161
yokesh-3 <yokeshb7@gmail.com>
2025-10-26 18:34:50 +0530

    feat: added wifi c file

---
+++ wifi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char str1[20];
+    char str2[20];
+    int flag = 0;
+    printf("Enter string1:");
+    gets(str1);
+    printf("Enter string2:");
+    gets(str2);
+}
+
+
